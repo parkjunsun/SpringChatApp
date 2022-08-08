78266b4d0a6784324f769dbad47d6bb99cd4fc48
parkjunsun <qkrwnstns52@naver.com>
2022-08-08 22:26:50 +0900

    2022.08.08 commit

--- src/main/java/com/cos/chatapp/ChatController.java
+++ src/main/java/com/cos/chatapp/ChatController.java
@@ -9,18 +9,21 @@ import reactor.core.scheduler.Schedulers;
 
 import java.time.LocalDateTime;
 
+
 @RequiredArgsConstructor
 @RestController // 데이터 리턴 서버
 public class ChatController {
 
     private final ChatRepository chatRepository;
 
+    @CrossOrigin
     @GetMapping(value = "/sender/{sender}/receiver/{receiver}", produces = MediaType.TEXT_EVENT_STREAM_VALUE) // MediaType.TEXT_EVENT_STREAM_VALUE -> SSE 프로토콜
     public Flux<Chat> getMsg(@PathVariable String sender, @PathVariable String receiver) { //Flux -> 데이터를 지속적으로 끝없이 흘려보내고 있음(화면 보면 계속 로딩중인데, 채팅 받을 준비를 하고 있는 느낌임)
          return chatRepository.mFindBySender(sender, receiver)
                 .subscribeOn(Schedulers.boundedElastic());
     }
 
+    @CrossOrigin
     @PostMapping("/chat")
     public Mono<Chat> setMsg(@RequestBody Chat chat) {  //save 한 데이터가 질 저장됬나 보고 싶어서 Mono 타입으로 return한것
         chat.setCreatedAt(LocalDateTime.now());
